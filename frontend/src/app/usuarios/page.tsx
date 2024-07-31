6c102f2364cf3c8e03eaa84335da49e51795a5bc
DiegoH3nr1que <diegohenrique123012@gmail.com>
2024-07-31 15:37:14 -0300

    style(): changes in user page

--- frontend/src/app/usuarios/page.tsx
+++ frontend/src/app/usuarios/page.tsx
@@ -127,7 +127,7 @@ export default function userPage() {
     <div className="h-screen flex flex-col">
       <div className="flex-1 flex">
         <Aside />
-        <main className="flex-1 flex flex-col p-6 bg-gradient-to-r from-gray-400 via-gray-300 to-gray-400">
+        <main className="flex-1 flex flex-col p-6 bg-gradient-to-r from-gray-500 via-gray-400 to-gray-300">
           <header className="text-center p-4 rounded-md mb-6">
             <h1 className="text-4xl font-bold uppercase text-blue-900">
               Sistema de Gestão de Manutenção
