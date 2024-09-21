e08ad1c6c19e4f2f07f5b471b1ad49af638eebde
Diego <diegohenrique123012@gmail.com>
2024-09-21 10:07:26 -0300

    refactor(components): modifications to header.ts to make it responsive

--- frontend_mobile/src/components/header.tsx
+++ frontend_mobile/src/components/header.tsx
@@ -3,26 +3,32 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 interface HeaderProps {
-    onNotificationPress: () => void;
-    onUserPress: () => void;
+  onNotificationPress: () => void;
+  onUserPress: () => void;
 }
 
-const Header: React.FC<HeaderProps> = ({ onNotificationPress, onUserPress }) => {
-    return (
-        <View className="flex-row items-center justify-between my-5 p-4">
-            <Text className="text-xl font-bold">
-                SISTEMA DE GESTÃO DE MANUTENÇÃO
-            </Text>
-            <View className="flex-row">
-                <TouchableOpacity onPress={onUserPress} style={{ marginRight: 20 }}>
-                    <Ionicons name="person-circle-outline" size={30} color="black" />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={onNotificationPress}>
-                    <Ionicons name="notifications-outline" size={30} color="black" />
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
+const Header: React.FC<HeaderProps> = ({
+  onNotificationPress,
+  onUserPress,
+}) => {
+  return (
+    <View className="flex-row items-center justify-between my-5 px-4">
+      <Text
+        className="text-lg font-bold flex-wrap text-center"
+        style={{ flex: 1 }}
+      >
+        SISTEMA DE GESTÃO DE MANUTENÇÃO
+      </Text>
+      <View className="flex-row">
+        <TouchableOpacity onPress={onUserPress} style={{ marginRight: 20 }}>
+          <Ionicons name="person-circle-outline" size={30} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onNotificationPress}>
+          <Ionicons name="notifications-outline" size={30} color="black" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
 
 export default Header;
