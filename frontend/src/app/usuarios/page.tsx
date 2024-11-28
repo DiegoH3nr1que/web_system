bc6fd3806e631b412e3b3a76a76b369e1072a1c1
DiegoH3nr1que <diegohenrique123012@gmail.com>
2024-11-28 12:37:12 -0300

    style(usuários): modifications in style the userPage

--- frontend/src/app/usuarios/page.tsx
+++ frontend/src/app/usuarios/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Aside } from "../components/aside";
@@ -41,7 +41,11 @@ export default function UserPage() {
   }, []);
 
   // Lidar com a criação de novos usuários
-  const handleCreateUser = async (userData: { username: string; password: string; role: string }) => {
+  const handleCreateUser = async (userData: {
+    username: string;
+    password: string;
+    role: string;
+  }) => {
     try {
       const response = await api.post("/users/register", userData);
       setUsers((prevUsers) => [...prevUsers, response.data]);
@@ -50,9 +54,11 @@ export default function UserPage() {
     }
   };
 
-
   // Lidar com a edição de usuários
-  const handleEditUser = async (userId: number, updatedData: { username: string; role: string }) => {
+  const handleEditUser = async (
+    userId: number,
+    updatedData: { username: string; role: string }
+  ) => {
     try {
       const response = await api.put(`/users/${userId}`, updatedData);
       setUsers((prevUsers) =>
@@ -85,14 +91,18 @@ export default function UserPage() {
           <Aside />
           <main className="flex-1 flex flex-col p-6 bg-background">
             <header className="text-left p-4 rounded-md mb-6 flex justify-between">
-              <h1 className="text-4xl font-bold uppercase text-foreground">Usuários</h1>
+              <h1 className="text-4xl font-bold uppercase text-foreground">
+                Usuários
+              </h1>
               <RealTimeClock />
             </header>
 
             <div className="flex-1">
               <div className="container mx-auto p-4 bg-background rounded-lg">
                 <div className="flex justify-between items-center mb-4">
-                  <h1 className="text-2xl font-bold text-foreground">Lista de usuários cadastrados</h1>
+                  <h1 className="text-2xl font-bold text-foreground">
+                    Lista de usuários cadastrados
+                  </h1>
                   <UserDialog
                     triggerLabel="Criar Usuário"
                     title="Criar Usuário"
@@ -109,20 +119,27 @@ export default function UserPage() {
                       columns={columns}
                       data={users}
                       actions={(item) => (
-                        <div className="flex space-x-2">
+                        <div className="flex justify-center items-center gap-4">
                           <UserDialog
-                            triggerLabel={<FaEdit />}
+                            triggerLabel={
+                              <FaEdit />
+                            }
                             title="Editar Usuário"
-                            user={{ username: item.username, role: item.role }}
+                            user={{
+                              username: item.username,
+                              role: item.role,
+                            }}
                             onSubmit={(updatedData) =>
                               handleEditUser(item.id, updatedData)
                             }
                           />
+
                           <button
                             onClick={() => handleDeleteUser(item.id)}
-                            className="text-red-500"
+                            className="text-red-500 hover:text-red-700 transition-colors"
+                            title="Excluir Usuário"
                           >
-                            <FaTrash />
+                            <FaTrash className="cursor-pointer" />
                           </button>
                         </div>
                       )}
